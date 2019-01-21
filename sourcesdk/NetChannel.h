5b0a5cd4d0977590ddcd4ae6caa9612624041486
Lewis <25719980+lewez@users.noreply.github.com>
2019-01-21 12:16:20 +0000

    Easier method of getting server address

--- sourcesdk/NetChannel.h
+++ sourcesdk/NetChannel.h
@@ -17,70 +17,11 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 #ifndef NETCHANNEL_H
 #define NETCHANNEL_H
 
-#include <cinttypes>
-
 namespace glt::ssdk {
-	class NetAdr;
-
 	class NetChannel {
 		public:
 		virtual void pad_0() = 0;
-		virtual void pad_1() = 0;
-		virtual void pad_2() = 0;
-		virtual void pad_3() = 0;
-		virtual void pad_4() = 0;
-		virtual void pad_5() = 0;
-		virtual void pad_6() = 0;
-		virtual void pad_7() = 0;
-		virtual void pad_8() = 0;
-		virtual void pad_9() = 0;
-		virtual void pad_10() = 0;
-		virtual void pad_11() = 0;
-		virtual void pad_12() = 0;
-		virtual void pad_13() = 0;
-		virtual void pad_14() = 0;
-		virtual void pad_15() = 0;
-		virtual void pad_16() = 0;
-		virtual void pad_17() = 0;
-		virtual void pad_18() = 0;
-		virtual void pad_19() = 0;
-		virtual void pad_20() = 0;
-		virtual void pad_21() = 0;
-		virtual void pad_22() = 0;
-		virtual void pad_23() = 0;
-		virtual void pad_24() = 0;
-		virtual void pad_25() = 0;
-		virtual void pad_26() = 0;
-		virtual void pad_27() = 0;
-		virtual void pad_28() = 0;
-		virtual void pad_29() = 0;
-		virtual void pad_30() = 0;
-		virtual void pad_31() = 0;
-		virtual void pad_32() = 0;
-		virtual void pad_33() = 0;
-		virtual void pad_34() = 0;
-		virtual void pad_35() = 0;
-		virtual void pad_36() = 0;
-		virtual void pad_37() = 0;
-		virtual void pad_38() = 0;
-		virtual void pad_39() = 0;
-		virtual void pad_40() = 0;
-		virtual void pad_41() = 0;
-		virtual void pad_42() = 0;
-		virtual void pad_43() = 0;
-		virtual void pad_44() = 0;
-		virtual void pad_45() = 0;
-		virtual void pad_46() = 0;
-		virtual void pad_47() = 0;
-		virtual void pad_48() = 0;
-		virtual NetAdr* GetRemoteAddress() = 0;
-	};
-
-	class NetAdr {
-		public:
-		int type;
-		std::uint8_t ip[4];
-		std::uint16_t port;
+		virtual const char* GetAddress() = 0;
 	};
 }
 
