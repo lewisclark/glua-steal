a0198eea45f012abf323fe8eefad698640bfc963
Lewis <25719980+lewez@users.noreply.github.com>
2019-01-22 14:54:09 +0000

    Add a few missing reserved windows filenames

--- file.cpp
+++ file.cpp
@@ -56,8 +56,8 @@ static std::vector<std::string> reserved {
 };
 #elif (defined(OS_WINDOWS))
 static std::vector<std::string> reserved {
-	"con", "prn", "aux", "nul", "com1", "com2", "com3", "com4", "com5",
-	"com6", "com7", "com8", "com9", "com10", "lpt1", "lpt2", "lpt3",
+	"con", "prn", "aux", "nul", "com0", "com1", "com2", "com3", "com4", "com5",
+	"com6", "com7", "com8", "com9", "com10", "lpt0", "lpt1", "lpt2", "lpt3",
 	"lpt4", "lpt5", "lpt6", "lpt7", "lpt8", "lpt9", "lpt10"
 };
 #endif
