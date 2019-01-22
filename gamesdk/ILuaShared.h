d71cf6280c9e507f19bd0c27761eb3e50ae83405
Lewis <25719980+lewez@users.noreply.github.com>
2019-01-22 18:09:44 +0000

    ILuaShared

--- gamesdk/ILuaShared.h
+++ gamesdk/ILuaShared.h
@@ -17,9 +17,20 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 #ifndef ILUASHARED_H
 #define ILUASHARED_H
 
+#include "GarrysMod/Lua/Interface.h"
+
 namespace glt::ssdk {
+	typedef GarrysMod::Lua::ILuaBase ILuaInterface;
+
 	class ILuaShared {
 		public:
+		virtual void pad_0() = 0;
+		virtual void pad_1() = 0;
+		virtual void pad_2() = 0;
+		virtual void pad_3() = 0;
+		virtual ILuaInterface* CreateLuaInterface(unsigned char, bool) = 0;
+		virtual void CloseLuaInterface(ILuaInterface*) = 0;
+		virtual ILuaInterface* GetLuaInterface(unsigned char) = 0;
 	};
 
 	extern ILuaShared* g_luashared;
