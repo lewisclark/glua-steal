c20dd33b9566883993e62d1de06d2fdefd2511dc
Lewis <25719980+lewez@users.noreply.github.com>
2019-01-28 12:32:55 +0000

    Update file tests

--- src/tests/file.cpp
+++ src/tests/file.cpp
@@ -4,12 +4,12 @@
 
 using namespace glt::file;
 
-TEST_CASE("file", "[file]") {
+TEST_CASE("Filename sanitization", "[file]") {
 	REQUIRE(SanitizeLuaFilePath("../../../hello.lua") == "hello.lua");
 	REQUIRE(SanitizeLuaFilePath("../../../dir/../../dir/../../../test.lua") == "dir/dir/test.lua");
 	REQUIRE(SanitizeLuaFilePath("cat") == "cat.lua");
 	REQUIRE(SanitizeLuaFilePath("") == "noname.lua");
-	REQUIRE(SanitizeLuaFilePath(".lua") == "noname.lua");
 	REQUIRE(SanitizeLuaFilePath(".") == "noname.lua");
-	REQUIRE(SanitizeLuaFilePath("../dir/////test//./../././myfile.txt") == "dir/test/myfile.txt");
+	REQUIRE(SanitizeLuaFilePath("../dir/////test//./../././myfile.txt") == "dir/test/myfile.lua");
+	REQUIRE(SanitizeLuaFilePath(".lua") == "noname.lua");
 }
