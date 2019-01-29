0a7c33b9c1101c56070a87cd2458dfedf315d54f
Lewis <25719980+lewez@users.noreply.github.com>
2019-01-29 10:41:56 +0000

    Use catch2 submodule instead of local header

--- src/tests/file.cpp
+++ src/tests/file.cpp
@@ -14,7 +14,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 
 
-#include "catch.hpp"
+#include <catch2/catch.hpp>
 
 #include "file/file.hpp"
 
