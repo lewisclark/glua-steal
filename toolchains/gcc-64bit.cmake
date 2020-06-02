bed4741657d68ca65c123fcde2c5b6ea35620d83
c4fe <me@c4fe.io>
2020-06-02 19:40:02 +0100

    msvc toolchain and better name

---
+++ toolchains/gcc-64bit.cmake
@@ -0,0 +1,4 @@
+set(CMAKE_CXX_FLAGS -m64 -Wall -Wextra -pedantic -Wno-unused-variable)
+set(CMAKE_C_FLAGS -m64 -Wall -Wextra -pedantic -Wno-unused-variable)
+
+set(CMAKE_EXE_LINKER_FLAGS -m64)
---
+++ toolchains/msvc.cmake
@@ -0,0 +1,2 @@
+set(CMAKE_CXX_FLAGS /W4)
+set(CMAKE_C_FLAGS /W4)
