2c8ed97a5266fa8951cb2248c4d35a8e3890842c
Lewis Clark <me@lewisclark.dev>
2022-10-27 19:17:40 +0100

    Link libSystem.dylib

--- toolchains/gcc-32bit.cmake
+++ toolchains/gcc-32bit.cmake
@@ -2,3 +2,5 @@ set(CMAKE_CXX_FLAGS "-m32 -Wall -Wextra -pedantic -Wno-unused-variable")
 set(CMAKE_C_FLAGS "-m32 -Wall -Wextra -pedantic -Wno-unused-variable")
 
 set(CMAKE_EXE_LINKER_FLAGS "-m32")
+
+target_link_libraries(gluasteal System)
