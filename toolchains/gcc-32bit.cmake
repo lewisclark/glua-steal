04d2788b81fe8d941d9be4b9e353cbe1271dcf58
Lewis Clark <me@lewisclark.dev>
2022-10-27 19:39:20 +0100

    Add prefix

--- toolchains/gcc-32bit.cmake
+++ toolchains/gcc-32bit.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_CXX_FLAGS "-m32 -Wall -Wextra -pedantic -Wno-unused-variable -lSystem")
-set(CMAKE_C_FLAGS "-m32 -Wall -Wextra -pedantic -Wno-unused-variable -lSystem")
+set(CMAKE_CXX_FLAGS "-m32 -Wall -Wextra -pedantic -Wno-unused-variable -llibSystem")
+set(CMAKE_C_FLAGS "-m32 -Wall -Wextra -pedantic -Wno-unused-variable -llibSystem")
 
-set(CMAKE_EXE_LINKER_FLAGS "-m32 -lSystem")
+set(CMAKE_EXE_LINKER_FLAGS "-m32 -llibSystem")
