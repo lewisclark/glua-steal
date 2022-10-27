c43b6b4fb6c7a89776666e1ca9649f16b5563df1
Lewis Clark <me@lewisclark.dev>
2022-10-27 22:12:17 +0100

    Link options

--- toolchains/gcc-macos-32bit.cmake
+++ toolchains/gcc-macos-32bit.cmake
@@ -2,3 +2,5 @@ set(CMAKE_CXX_FLAGS "-lSystem -m32 -Wall -Wextra -pedantic -Wno-unused-variable"
 set(CMAKE_C_FLAGS "-lSystem -m32 -Wall -Wextra -pedantic -Wno-unused-variable")
 
 set(CMAKE_EXE_LINKER_FLAGS "-lSystem -m32")
+
+add_link_options("-lSystem")
