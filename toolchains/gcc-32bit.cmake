1446c4f3e05d4f1c40a9e2b5323866a47a27d6ec
Lewis Clark <me@lewisclark.dev>
2022-10-27 19:19:43 +0100

    Temp link System

--- toolchains/gcc-32bit.cmake
+++ toolchains/gcc-32bit.cmake
@@ -2,5 +2,3 @@ set(CMAKE_CXX_FLAGS "-m32 -Wall -Wextra -pedantic -Wno-unused-variable")
 set(CMAKE_C_FLAGS "-m32 -Wall -Wextra -pedantic -Wno-unused-variable")
 
 set(CMAKE_EXE_LINKER_FLAGS "-m32")
-
-target_link_libraries(gluasteal System)
