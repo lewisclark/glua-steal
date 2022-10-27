5e16a48cfd4de960406a3a16f036fd7d1c8fca11
Lewis Clark <me@lewisclark.dev>
2022-10-27 19:34:57 +0100

    Update flags

--- toolchains/gcc-32bit.cmake
+++ toolchains/gcc-32bit.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_CXX_FLAGS "-m32 -Wall -Wextra -pedantic -Wno-unused-variable")
-set(CMAKE_C_FLAGS "-m32 -Wall -Wextra -pedantic -Wno-unused-variable")
+set(CMAKE_CXX_FLAGS "-m32 -Wall -Wextra -pedantic -Wno-unused-variable -lSystem")
+set(CMAKE_C_FLAGS "-m32 -Wall -Wextra -pedantic -Wno-unused-variable -lSystem")
 
-set(CMAKE_EXE_LINKER_FLAGS "-m32")
+set(CMAKE_EXE_LINKER_FLAGS "-m32 -lSystem")
