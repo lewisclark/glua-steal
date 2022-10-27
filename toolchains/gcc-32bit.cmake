b9d61d4af030cb705a4e31baed4655d5f4108ef4
Lewis Clark <me@lewisclark.dev>
2022-10-27 21:52:15 +0100

    Reverse gcc-32bit toolchain

--- toolchains/gcc-32bit.cmake
+++ toolchains/gcc-32bit.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_CXX_FLAGS "-lSystem -m32 -Wall -Wextra -pedantic -Wno-unused-variable")
-set(CMAKE_C_FLAGS "-lSystem -m32 -Wall -Wextra -pedantic -Wno-unused-variable")
+set(CMAKE_CXX_FLAGS "-m32 -Wall -Wextra -pedantic -Wno-unused-variable")
+set(CMAKE_C_FLAGS "-m32 -Wall -Wextra -pedantic -Wno-unused-variable")
 
-set(CMAKE_EXE_LINKER_FLAGS "-lSystem -m32")
+set(CMAKE_EXE_LINKER_FLAGS "-m32")
---
+++ toolchains/gcc-macos-32bit.cmake
@@ -0,0 +1,4 @@
+set(CMAKE_CXX_FLAGS "-lSystem -m32 -Wall -Wextra -pedantic -Wno-unused-variable")
+set(CMAKE_C_FLAGS "-lSystem -m32 -Wall -Wextra -pedantic -Wno-unused-variable")
+
+set(CMAKE_EXE_LINKER_FLAGS "-lSystem -m32")
