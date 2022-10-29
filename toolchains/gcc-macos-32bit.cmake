437818284ff0f362a9e38dd15f3840f7f8a42d45
Lewis Clark <me@lewisclark.dev>
2022-10-29 17:41:24 +0100

    Set arch to i386

--- toolchains/gcc-macos-32bit.cmake
+++ toolchains/gcc-macos-32bit.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_CXX_FLAGS "-lSystem -m32 -Wall -Wextra -pedantic -Wno-unused-variable")
-set(CMAKE_C_FLAGS "-lSystem -m32 -Wall -Wextra -pedantic -Wno-unused-variable")
+set(CMAKE_CXX_FLAGS "-lSystem -m32 -Wall -Wextra -pedantic -Wno-unused-variable -arch i386")
+set(CMAKE_C_FLAGS "-lSystem -m32 -Wall -Wextra -pedantic -Wno-unused-variable -arch i386")
 
-set(CMAKE_EXE_LINKER_FLAGS "-lSystem -m32")
+set(CMAKE_EXE_LINKER_FLAGS "-lSystem -m32 -arch i386")
 
 add_link_options("-lSystem")
