fcab91e2c8bd06a82418af145c2eee4f2162dd81
Lewis Clark <me@lewisclark.dev>
2022-10-29 17:50:31 +0100

    Set sysroot

--- toolchains/gcc-macos-32bit.cmake
+++ toolchains/gcc-macos-32bit.cmake
@@ -1,5 +1,5 @@
-set(CMAKE_CXX_FLAGS "-lSystem -m32 -Wall -Wextra -pedantic -Wno-unused-variable -arch i386")
-set(CMAKE_C_FLAGS "-lSystem -m32 -Wall -Wextra -pedantic -Wno-unused-variable -arch i386")
+set(CMAKE_CXX_FLAGS "-lSystem -m32 -Wall -Wextra -pedantic -Wno-unused-variable -arch i386 --sysroot /Developer/SDKs/MacOSX10.15.sdk/")
+set(CMAKE_C_FLAGS "-lSystem -m32 -Wall -Wextra -pedantic -Wno-unused-variable -arch i386 --sysroot /Developer/SDKs/MacOSX10.15.sdk/")
 
 set(CMAKE_EXE_LINKER_FLAGS "-lSystem -m32 -arch i386")
 
