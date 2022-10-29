6aba6e4fca097802af560902fa81f010914a822e
Lewis Clark <me@lewisclark.dev>
2022-10-29 18:12:59 +0100

    CMAKE_OSX_ARCHITECTURES

--- toolchains/gcc-macos-32bit.cmake
+++ toolchains/gcc-macos-32bit.cmake
@@ -1,8 +1,9 @@
-set(CMAKE_CXX_FLAGS "-m32 -Wall -Wextra -pedantic -Wno-unused-variable -arch i386 --sysroot /Developer/SDKs/MacOSX10.15.sdk/")
-set(CMAKE_C_FLAGS "-m32 -Wall -Wextra -pedantic -Wno-unused-variable -arch i386 --sysroot /Developer/SDKs/MacOSX10.15.sdk/")
+set(CMAKE_CXX_FLAGS "-m32 -Wall -Wextra -pedantic -Wno-unused-variable -arch i386")
+set(CMAKE_C_FLAGS "-m32 -Wall -Wextra -pedantic -Wno-unused-variable -arch i386")
 
 set(CMAKE_EXE_LINKER_FLAGS "-m32 -arch i386")
 
 set(CMAKE_OSX_SYSROOT /Developer/SDKs/MacOSX10.15/)
-
 set(CMAKE_OSX_DEPLOYMENT_TARGET "10.15")
+
+set(CMAKE_OSX_ARCHITECTURES i386)
