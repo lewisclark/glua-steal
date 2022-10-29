bd4d4edf75d3fb4c349b631f1cdfa0eff6d186f3
Lewis Clark <me@lewisclark.dev>
2022-10-29 17:54:43 +0100

    Set CMake sysroot and deployment target

--- toolchains/gcc-macos-32bit.cmake
+++ toolchains/gcc-macos-32bit.cmake
@@ -3,4 +3,8 @@ set(CMAKE_C_FLAGS "-lSystem -m32 -Wall -Wextra -pedantic -Wno-unused-variable -a
 
 set(CMAKE_EXE_LINKER_FLAGS "-lSystem -m32 -arch i386")
 
+set(CMAKE_OSX_SYSROOT /Developer/SDKs/MacOSX10.15/)
+
+set(CMAKE_OSX_DEPLOYMENT_TARGET "10.15")
+
 add_link_options("-lSystem")
