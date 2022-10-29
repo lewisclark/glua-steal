f8a43596921c6532bb308c9891f4c94569a39fce
Lewis Clark <me@lewisclark.dev>
2022-10-29 18:22:59 +0100

    Remove sysroot and deployment target flags

--- toolchains/gcc-macos-32bit.cmake
+++ toolchains/gcc-macos-32bit.cmake
@@ -1,8 +1,4 @@
 set(CMAKE_CXX_FLAGS "-Wall -Wextra -pedantic -Wno-unused-variable")
 set(CMAKE_C_FLAGS "-Wall -Wextra -pedantic -Wno-unused-variable")
 
-#set(CMAKE_EXE_LINKER_FLAGS "-m32 -arch i386")
-
-set(CMAKE_OSX_SYSROOT "/Developer/SDKs/MacOSX10.15/")
-set(CMAKE_OSX_DEPLOYMENT_TARGET "10.15")
 set(CMAKE_OSX_ARCHITECTURES "i386")
