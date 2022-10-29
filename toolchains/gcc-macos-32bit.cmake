b7006ee87eb948575377c4012a2ba6fb0f9dcfc3
Lewis Clark <me@lewisclark.dev>
2022-10-29 17:57:36 +0100

    Remove -lSystem flag

--- toolchains/gcc-macos-32bit.cmake
+++ toolchains/gcc-macos-32bit.cmake
@@ -1,10 +1,8 @@
-set(CMAKE_CXX_FLAGS "-lSystem -m32 -Wall -Wextra -pedantic -Wno-unused-variable -arch i386 --sysroot /Developer/SDKs/MacOSX10.15.sdk/")
-set(CMAKE_C_FLAGS "-lSystem -m32 -Wall -Wextra -pedantic -Wno-unused-variable -arch i386 --sysroot /Developer/SDKs/MacOSX10.15.sdk/")
+set(CMAKE_CXX_FLAGS "-m32 -Wall -Wextra -pedantic -Wno-unused-variable -arch i386 --sysroot /Developer/SDKs/MacOSX10.15.sdk/")
+set(CMAKE_C_FLAGS "-m32 -Wall -Wextra -pedantic -Wno-unused-variable -arch i386 --sysroot /Developer/SDKs/MacOSX10.15.sdk/")
 
-set(CMAKE_EXE_LINKER_FLAGS "-lSystem -m32 -arch i386")
+set(CMAKE_EXE_LINKER_FLAGS "-m32 -arch i386")
 
 set(CMAKE_OSX_SYSROOT /Developer/SDKs/MacOSX10.15/)
 
 set(CMAKE_OSX_DEPLOYMENT_TARGET "10.15")
-
-add_link_options("-lSystem")
