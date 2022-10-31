adaadaeacc99ab5f6106bca2535a6dcea844ab86
Lewis Clark <me@lewisclark.dev>
2022-10-31 09:14:13 +0000

    Correctly set cache variable CMAKE_OSX_ARCHITECTURES

--- toolchains/gcc-macos-i386.cmake
+++ toolchains/gcc-macos-i386.cmake
@@ -1,4 +1,4 @@
 set(CMAKE_CXX_FLAGS "-Wall -Wextra -pedantic -Wno-unused-variable")
 set(CMAKE_C_FLAGS "-Wall -Wextra -pedantic -Wno-unused-variable")
 
-set(CMAKE_OSX_ARCHITECTURES "i386")
+set(CMAKE_OSX_ARCHITECTURES "i386" CACHE STRING "" FORCE)
