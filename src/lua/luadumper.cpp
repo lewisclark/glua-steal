0e28b521a60d0574504f34ab4d88fca208dbab96
Lewis Clark <me@lewisclark.dev>
2022-11-24 12:37:38 +0000

    Replace \n with std::endl

--- src/lua/luadumper.cpp
+++ src/lua/luadumper.cpp
@@ -68,9 +68,9 @@ void glt::lua::IoThread() {
 					}
 
 					auto of = std::ofstream(path, glt::config::GetConfig().stealer_write_mode);
-					of << "-- " << sanitized_filename << "\n";
-					of << "-- Retrieved by https://github.com/lewisclark/glua-steal\n";
-					of << entry.code << "\n\n";
+					of << "-- " << sanitized_filename << std::endl;
+					of << "-- Retrieved by https://github.com/lewisclark/glua-steal" << std::endl;
+					of << entry.code;
 				}
 
 				shared_entries.clear();
